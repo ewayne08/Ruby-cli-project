1fb15e631dee44f84281c504192b6d183e8ac717
Ethan <ethanchristianwayne@gmail.com>
2020-11-11 17:20:01 -0500

    message

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,6 +5,30 @@ require_relative "./lib/bugatti_models"
 class BugattiModels::CLI   
 
     def call 
+        BugattiModels::Scraper.new.show_models
         puts "Find out about Bugatti models!"
+        start
+    end
+
+    def start 
+        puts ""
+        puts "What model would you like to see? 1-5?"
+        input = gets.strip.to_i
+
+        print_model(input)
+
+        puts ""
+        puts "Would you like to see another model? Enter Y or N"
+
+        input = gets.strip.downcase
+        if input == "y"
+            start
+        elsif input == "n"
+            puts "Thank you! I hope you enjoyed!"
+            exit
+        else
+            puts "I don't know what you're asking."
+            start
+        end
     end
 end
